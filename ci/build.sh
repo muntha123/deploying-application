be3d22e7fb76e7fedd08f85c9143db44eafea792
muntha123 <47027255+muntha123@users.noreply.github.com>
2019-05-06 18:26:46 +0530

    Create build.sh

---
+++ ci/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -x
+
+target="cf api $API_ENDPOINT --skip-ssl-validation"
+#echo $target
+eval $target
+
+echo "Login....."
+login="cf auth $USERNAME $PASSWORD"
+#echo $login
+eval $login
+
+echo "Target Org and Space"
+org_space="cf target -o $ORG -s $SPACE"
+eval $org_space
+
+cd deploying-application/application/
+
+cf push APPNAME -p ./applicatin.war -i 1
+
+
+
+
+#curl -u "${artifactory_userid}":"${artifactory_pass}" -X PUT  "${artifactory_path}"jar/ -T ../build_folder/Conversion-Service-$project_version.jar -k
